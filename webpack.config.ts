1550d3cd45faf032d4d66a6f33f1da437bccfe90
Steve <stevejamesdaniels@outlook.com>
2021-01-17 23:26:23 +0000

    Reformatted file

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,12 @@
 import path from 'path'
-import webpack, {Configuration} from 'webpack'
+import webpack, { Configuration } from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
-import {TsconfigPathsPlugin} from 'tsconfig-paths-webpack-plugin'
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin'
 
 const webpackConfig = (env): Configuration => ({
     entry: './src/index.tsx',
-    ...(env.production || !env.development ? {} : {devtool: 'eval-source-map'}),
+    ...(env.production || !env.development ? {} : { devtool: 'eval-source-map' }),
     resolve: {
         extensions: ['.ts', '.tsx', '.js'],
         //TODO waiting on https://github.com/dividab/tsconfig-paths-webpack-plugin/issues/61
